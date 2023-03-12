dfeef364c38625505ab769b5158e82194b499f36
SaharGhadampoorHafezi <saharghdampoor@gmail.com>
2023-03-12 19:31:40 +0330

    install next.js and create first template

---
+++ src/app/head.jsx
@@ -0,0 +1,10 @@
+export default function Head() {
+    return (
+      <>
+        <title>Google of Sahar</title>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <meta name="description" content="Google by Sahar :)" />
+        <link rel="icon" href="/favicon.svg" />
+      </>
+    )
+  }
