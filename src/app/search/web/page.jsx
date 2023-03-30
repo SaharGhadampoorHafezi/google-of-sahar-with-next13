3f76728eac9478d8cb92c78f9df1c20a5e1fb37c
SaharGhadampoorHafezi <saharghdampoor@gmail.com>
2023-03-31 00:32:17 +0430

    handle possible errors

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,13 +1,32 @@
+import Link from "next/link";
 import React from "react";
 
-export default async function webSearchPage({searchParams}) {
+export default async function webSearchPage({ searchParams }) {
   const response = await fetch(
     //https://developers.google.com/custom-search/v1/using_rest?hl=en&authuser=2
     `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
   );
+  if (!response.ok) {
+    throw new Error();
+  }
   const data = await response.json();
   console.log(data);
   const results = data.items;
+  if (!results) {
+    return (
+      <div className="flex flex-col justify-center items-center pt-10">
+        <h1 className="text-3xl mb-4">no result find</h1>
+        <p className="text-lg">
+          try search something else or go back page 
+          <Link className="text-blue-500" href="/">
+            home
+          </Link>{" "}
+          page
+        </p>
+      </div>
+    );
+  }
+
   return <>{results && results.map((result) => <h1>{result.title}</h1>)}</>;
 }
 
